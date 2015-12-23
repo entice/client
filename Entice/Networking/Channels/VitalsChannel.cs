fe7dfef17f5b46d4c4bbedb0dc3aa367b776afcb
Alexander Rummel <rummel.alexander@outlook.com>
2015-12-23 14:46:58 +0100

    Implemented Vitals Channel Implemented entity:resurrected and entity:dead

---
+++ Entice/Networking/Channels/VitalsChannel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Entice.Base;
+using Entice.Entities;
+using GuildWarsInterface;
+using GuildWarsInterface.Declarations;
+using Newtonsoft.Json.Linq;
+
+namespace Entice.Channels
+{
+    internal sealed class VitalsChannel : Channel
+    {
+        internal VitalsChannel() : base("vitals") { }
+
+        public override void HandleMessage(Message message)
+        {
+            switch (message.Event)
+            {
+                case "entity:resurrected":
+                {
+                    
+                }
+                    break;
+                case "entity:dead":
+                {
+                    Player myId =
+                        Entity.Entities.Values.OfType<Player>().First(p => p.Character == Game.Player.Character);
+
+                    Entity.Players.First(p => p.Character == Game.Player.Character).Character.Status =
+                        CreatureStatus.Dead;
+                }
+                    break;
+            }
+        }
+    }
+}
